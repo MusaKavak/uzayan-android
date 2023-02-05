32385d60bf97be832f86f83ee3d613080a830619
MusaKavak <musa0kavak@gmail.com>
2023-02-05 18:40:07 +0300

    Media Session Manager Optimized

--- app/src/main/java/dev/musakavak/uzayan/services/NLService.kt
+++ app/src/main/java/dev/musakavak/uzayan/services/NLService.kt
@@ -1,11 +1,33 @@
 package dev.musakavak.uzayan.services
 
+import android.media.MediaMetadata
+import android.os.Bundle
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
 
 class NLService:NotificationListenerService() {
+    private val tag = "NotificationListener"
     override fun onListenerConnected() {
         super.onListenerConnected()
 
         println("Notification Lıstener Connected")
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+
+    }
+
+    fun printBundle(bundle: Bundle?,message:Any) {
+        val extras = bundle?.keySet()
+        Log.w(tag, "-------------------------------------------------$message")
+        extras?.forEach {
+            Log.i(tag, "Key: $it ----------- Value: ${bundle.get(it)}")
+        }
+        Log.w(tag, "-------------------------------------------------")
     }
 }
--- app/src/main/java/dev/musakavak/uzayan/utils/MediaSessionManager.kt
+++ app/src/main/java/dev/musakavak/uzayan/utils/MediaSessionManager.kt
@@ -2,40 +2,55 @@ package dev.musakavak.uzayan.utils
 
 import android.content.ComponentName
 import android.content.Context
-import android.media.MediaMetadata
 import android.media.session.MediaController
 import android.media.session.MediaSessionManager
 import android.media.session.PlaybackState
-import android.os.Bundle
-import android.util.Log
 import dev.musakavak.uzayan.services.NLService
 
-class MediaSessionManager(private val context: Context) {
-    private val tag = "MediaSessionManager"
-    fun listen() {
+class MediaSessionManager(context: Context) {
+    private val manager: MediaSessionManager =
+        context.getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager
+    private val componentName = ComponentName(context, NLService::class.java)
+    private var counter = 0
 
-        var c = 0
-        val manager = context.getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager
+    private data class PreviousController(
+        val controller: MediaController,
+        val callback: MediaController.Callback
+    )
+
+    private var previousControllers: MutableList<PreviousController> = mutableListOf()
+
+    fun listen() {
+        listenControllers(manager.getActiveSessions(componentName))
         manager.addOnActiveSessionsChangedListener({
-          it?.forEach {controller ->
-              controller.registerCallback(object : MediaController.Callback() {
-                  override fun onPlaybackStateChanged(state: PlaybackState?) {
-                      super.onPlaybackStateChanged(state)
-                     // printBundle(controller.metadata)
-                      println("Current: $c")
-                      c++
-                  }
-              })
-          }
-        }, ComponentName(context, NLService::class.java))
+            listenControllers(it)
+        }, componentName)
     }
 
-    fun printBundle(bundle:MediaMetadata?){
-        val extras = bundle?.keySet()
-        Log.w(tag,"-------------------------------------------------")
-        extras?.forEach {
-            Log.i(tag,"Key: $it ----------- Value: ${bundle.getString(it)}")
+    private fun listenControllers(controllers: List<MediaController>?) {
+        previousControllers.forEach {
+            it.controller.unregisterCallback(it.callback)
         }
-        Log.w(tag,"-------------------------------------------------")
+        previousControllers.clear()
+        controllers?.let {
+            println("ListSize:       " + it.size)
+            it.forEach { controller ->
+                val callback = createCallback(controller)
+                println("Token: " + controller.sessionToken)
+                previousControllers.add(PreviousController(controller,callback))
+                controller.registerCallback(callback)
+            }
+        }
+    }
+
+    private fun createCallback(controller: MediaController): MediaController.Callback {
+        return object : MediaController.Callback() {
+            override fun onPlaybackStateChanged(state: PlaybackState?) {
+                super.onPlaybackStateChanged(state)
+                println(counter)
+                counter++
+            }
+        }
     }
 }
+
